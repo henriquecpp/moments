676f8591b13120329820eb7412821066bdb35da8
henriquecpp <henriquemarino48@gmail>
2022-12-13 21:20:17 -0300

    implemented method to add data to database

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+import { Moment } from 'src/app/interfaces/moment';
+import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -8,13 +14,27 @@ import { Component, OnInit } from '@angular/core';
 export class NewMomentComponent implements OnInit {
   btnText = "Compartilhar"
 
-  constructor() { }
+  constructor(private momentService: MomentService,
+    private messagesService: MessagesService,
+    private router: Router,
+    private titleService: Title
+  ) { this.titleService.setTitle("New Moment"); }
 
   ngOnInit(): void {
   }
 
-  createHandler(event: any){
-    console.log('foi');
+  async createHandler(moment: any){
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.descriptin);
+    formData.append('iamge', moment.image);
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    this.messagesService.add("Adicionado com sucesso!");
+
+    this.router.navigate(['/']);
   }
 
 }
